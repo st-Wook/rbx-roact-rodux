dcf253bdfddfff2eaf377528d5bd47a625fa5972
supertree-wook <phk09242@supertree.co>
2023-11-20 16:30:56 +0900

    Update join.lua

--- src/join.lua
+++ src/join.lua
@@ -1,16 +1,33 @@
 local function join(...)
-	local result = {}
+	local argCount = select("#", ...)
+	if argCount == 0 then
+		return {}
+	elseif argCount == 1 then
+		local new = table.clone(... or {})
+		setmetatable(new, nil) -- We don't want cloned metatables
+		return new
+	end
 
-	for i = 1, select("#", ...) do
+	local new
+	local first = select(1, ...)
+	if first ~= nil then
+		new = table.clone(first)
+		setmetatable(new, nil) -- We don't want cloned metatables
+	else
+		new = {}
+	end
+
+	for i = 2, argCount do
 		local source = select(i, ...)
+
 		if source ~= nil then
 			for key, value in pairs(source) do
-				result[key] = value
+				new[key] = value
 			end
 		end
 	end
 
-	return result
+	return new
 end
 
 return join
