4f8448efcd1d5d87b92e979d94c7f14b252336bf
supertree-wook <phk09242@supertree.co>
2024-01-12 12:26:14 +0900

    Update store provider

--- src/StoreProvider.lua
+++ src/StoreProvider.lua
@@ -19,7 +19,7 @@ end
 function StoreProvider:render()
 	return Roact.createElement(StoreContext.Provider, {
 		value = self.store,
-	}, Roact.oneChild(self.props[Roact.Children]))
+	}, self.props[Roact.Children])
 end
 
 return StoreProvider
